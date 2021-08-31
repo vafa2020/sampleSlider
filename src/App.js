43aa6f06cf98a9799d3f234e10784894fc3a209f
Mohammad Hossein Vafaee <vafaee.mh@gmail.com>
2021-08-31 22:27:34 +0430

    the finish slider app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import SliderApp from "./component/SliderApp";
+import { sliderData } from "./component/sliderData";
 
 function App() {
   return (
     <div className="App">
       <h2 className="title">slider-App</h2>
-      <SliderApp />
+      <SliderApp sliderData={sliderData} />
     </div>
   );
 }
--- src/component/SliderApp.js
+++ src/component/SliderApp.js
@@ -1,14 +1,34 @@
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
-import { sliderData } from "./sliderData";
 import { useState } from "react";
-const SliderApp = () => {
-  const [current, setCurrent] = useState(1);
+const SliderApp = ({ sliderData }) => {
+  const [current, setCurrent] = useState(0);
+  const length = sliderData.length;
+  if(!Array.isArray(sliderData) || sliderData.length <=0){
+      return null
+  }
+  //current.length=1,2,3,4,5
+  //index=0,1,2,3,4
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+  console.log(current);
+
   return (
     <div className="sliderApp">
-      <FaArrowAltCircleRight className="arrRight" />
-      <FaArrowAltCircleLeft className="arrLeft" />
-      {sliderData.map((item) => {
-        return <img key={item.id} className="image" src={item.source} />;
+      <FaArrowAltCircleRight className="arrRight" onClick={nextSlide} />
+      <FaArrowAltCircleLeft className="arrLeft" onClick={prevSlide} />
+      {sliderData.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className={current === index ? "slide active" : "slide"}
+          >
+            {current === index && <img className="image" src={item.source} />}
+          </div>
+        );
       })}
     </div>
   );
